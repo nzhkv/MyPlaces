dd8f42523ff3165af5877aeb6b752e7301968035
NZhukov <nzhukov.rnd@gmail.com>
2023-04-13 21:49:10 +0400

    settings

---
+++ MyPlaces/MainVC.swift
@@ -0,0 +1,43 @@
+//
+//  MainVC.swift
+//  MyPlaces
+//
+//  Created by Nikolay Zhukov on 12.04.2023.
+//
+
+import UIKit
+
+class MainVC: UITableViewController {
+    
+    let restorauntName = ["McDonalds", "ChaCha Time", "Back Door", "Black sea"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restorauntName.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = restorauntName[indexPath.row]
+        
+        cell.contentConfiguration = content
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
